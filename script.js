25d57c4600cd91c0978fe87fb662535862362ea2
david <cognet.david@hotmail.fr>
2023-01-06 15:47:41 +0100

    test de cut from codepen

---
+++ script.js
@@ -0,0 +1,11 @@
+// Create your variables here
+// ==========================================
+
+// ==========================================
+
+let paragraph = document.querySelector("#info");
+paragraph.innerText = `${numberOfSeasons} seasons,
+${numberOfEpisodes} episodes per season`;
+
+let numberOfSeasons = 12;
+let numberOfEpisodes = 6;
