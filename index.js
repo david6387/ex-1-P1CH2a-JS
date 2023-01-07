415576472d0814a437f1b9f2924a86585fd7f895
david <cognet.david@hotmail.fr>
2023-01-07 22:23:54 +0100

    test mise en forme

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 
 // ==========================================
 
+let numberOfSeasons = 12;
+let numberOfEpisodes = 6;
+
 let paragraph = document.querySelector("#info");
 paragraph.innerText = `${numberOfSeasons} seasons,
 ${numberOfEpisodes} episodes per season`;
-
-let numberOfSeasons = 12;
-let numberOfEpisodes = 6;
